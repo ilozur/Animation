8c4e090e91b09829b18a17cb02d0ad0661670a4b
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-11 15:09:22 +0300

    Animating gestures

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isEnabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    let letters = Array("Hello, SwiftUI")
     
     var body: some View {
         Button("Tap me") {
@@ -20,6 +23,41 @@ struct ContentView: View {
         .foregroundColor(.white)
         .clipShape(RoundedRectangle(cornerRadius: isEnabled ? 60 : 0))
         .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: isEnabled)
+        
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 25))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in
+                        dragAmount = .zero
+                    }
+            )
+            .animation(.spring(), value: dragAmount)
+        
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(isEnabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded {_ in
+                    withAnimation {
+                        dragAmount = .zero
+                        isEnabled.toggle()
+                    }
+            }
+        )
+        
     }
 }
     
