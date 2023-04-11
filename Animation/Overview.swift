afb8ad35db76ac9eff8fc4620311e4a574ef8427
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-11 13:45:38 +0300

    Animating bindings

--- Animation/Overview.swift
+++ Animation/Overview.swift
@@ -33,7 +33,6 @@ struct Overview: View {
         )
   
         Button("Tap me!") {
-//            animationAmount += 1
         }
         .padding(50)
         .background(.red)
@@ -52,6 +51,24 @@ struct Overview: View {
         )
         .onAppear {
             animationAmount = 2
+        }
+        
+        VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
     }
 }
